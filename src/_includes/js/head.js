d3fa31bceb1e533dbd84e0fde1a39ff729f2ab76
kmissett <kmissett77@gmail.com>
2024-10-17 08:46:23 -0400

    Moved noscript and color mode toggle JS into head, and removed them from scripts.js file.

---
+++ src/_includes/js/head.js
@@ -0,0 +1,24 @@
+/* remove any noscript items */
+const noscripts = document.querySelectorAll('.noscript')
+if (noscripts.length) noscripts.forEach(item => {item.classList.add('display-none')})
+
+/* color mode functions */
+function getColorMode() {
+    if(localStorage.currentColorMode) return localStorage.currentColorMode
+    if(window.matchMedia("prefers-color-scheme: light").matches) return "light"
+    return "dark"
+}
+
+function setColorMode(mode) {
+    localStorage.setItem("currentColorMode", mode)
+    mode === "light" ? document.documentElement.classList.add("light-mode") : document.documentElement.classList.remove("light-mode")
+}
+
+function changeColorMode() {
+    const currentColorMode = localStorage.currentColorMode || "dark"
+    currentColorMode === "dark" ? setColorMode("light") : setColorMode("dark")
+}
+
+setColorMode(getColorMode())
+const colorModeToggle = document.querySelectorAll('.color-mode-toggle')
+colorModeToggle.forEach(element => { element.addEventListener('click', changeColorMode)})
--- src/_includes/js/utilities.js
+++
@@ -1,3 +0,0 @@
-/* remove any noscript items */
-const noscripts = document.querySelectorAll('.noscript')
-if (noscripts.length) noscripts.forEach(item => {item.classList.add('display-none')})
